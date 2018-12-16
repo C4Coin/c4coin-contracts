15928999b43a7a0ea67ba1a151eb5fc13cffbb20
Tigran Avakyan <tigran.avakyan@icloud.com>
2018-12-16 01:49:25 -0800

    Adding FixedPoint library that works with ether units

---
+++ contracts/methodologies/libraries/FixedPointSafeMath.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+
+/**
+ * @title FixedPointSafeMath
+ * @dev Math operations on fixed point represented using ether units
+ */
+library FixedPointSafeMath {
+
+    using SafeMath for uint256;
+
+    /**
+    * @dev Multiplies two fixed point numbers, reverts on overflow.
+    * @note Must divide by 1e18 to move decimal place back.
+    */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a.mul(b).div(1 ether);
+    }
+
+    /**
+    * @dev Integer division of two fixed point numbers
+    * @note Must multiply by 1 ether to move left then divide.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a.mul(1 ether).div(b);
+    }
+
+    /**
+    * @dev Subtracts two fixed point numbers
+    */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a.sub(b);
+    }
+
+    /**
+    * @dev Adds two fixed point numbers
+    */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a.add(b);
+    }
+    
+}
